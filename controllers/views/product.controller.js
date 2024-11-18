b8ea06338bf0acdc4d831c79806acc5db07252e7
Vân Duy Quang <106808117+vanduyquang100@users.noreply.github.com>
2024-11-18 17:10:54 +0700

    Finish implement Homepage promotions (#9)

--- controllers/views/product.controller.js
+++ controllers/views/product.controller.js
@@ -20,12 +20,21 @@ class ProductViewController {
     }
   }
 
-  async getAllProducts(req) {
+  async getAllProducts(req, res) {
     try {
       const { page, limit, ...filter } = req.query;
       const products = await productService.getProducts(filter, page, limit);
       const categories = await productService.getCategories();
       return { ...products, categories };
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  async getPromotionProducts(res) {
+    try {
+      const products = await productService.getPromotionProducts();
+      return products;
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
